6b809bce4535b8fe7fc154b454ac8875da2d122d
sandaya <sandaya@gmail.com>
2024-02-29 20:34:22 -0500

    tmux colors

--- showColors.sh
+++ showColors.sh
@@ -32,3 +32,13 @@ for C in {0..255}; do
 done
 tput sgr0
 echo
+
+
+echo "tmux colors"
+for num in {0..255}; do
+    printf "%s\033[38;5;${num}mcolour${num}\033[0m \t";
+
+    if [ $(expr $((num+1)) % 8) -eq 0 ]; then
+        printf "\n"
+    fi
+done
